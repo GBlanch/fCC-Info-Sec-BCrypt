941aaf402b296d512c191a2b4c6c6148a637d4cf
Gerry Blanch <g.blandugar@gmail.com>
2024-01-21 10:00:40 -0500

    Synchronously hash and compare

--- server.js
+++ server.js
@@ -10,18 +10,24 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+/*
 const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log("Hash :" + hash);
     bcrypt.compare(myPlaintextPassword, hash, (err,res) => {
     console.log("Res :", res);
     });
 });
+*/
 
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log("Hash :" + hash);
 
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log("Res :", res);
 
 //END_SYNC
 
